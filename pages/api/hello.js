ac7ef0e59d3543681a58e7b26ade29c7772c4bdd
Saif ur rehman <m.awan.saifurrehman@gmail.com>
2022-11-30 17:44:28 -0500

    prisma and db connection

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from "@prisma/client";
+const prismaClient = new PrismaClient();
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const data = await prismaClient.user.findMany();
+  res.status(200).json({ name: "John Doe", data: data });
 }
