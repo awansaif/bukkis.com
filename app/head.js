4a7d71f04d7b077784f242bdb2c194e5b5b12ed9
Saif ur rehman <m.awan.saifurrehman@gmail.com>
2022-11-30 17:53:03 -0500

    Google search console

--- app/head.js
+++ app/head.js
@@ -4,6 +4,10 @@ export default function Head() {
       <title>Bukkis - Home</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
+      <meta
+        name="google-site-verification"
+        content="HQZFyFKNPsxJax5C3ADENDiS4IfEXK0Mh6djJUd_Eq4"
+      />
     </>
   );
 }
